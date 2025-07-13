cf5ecd40ba3b2495268c5c9d43ad52eee29c4a07
Ash Thx <210240255+AshleyThx@users.noreply.github.com>
2025-07-13 19:03:12 +0300

    refactor: change Action struct to use i32 instead of i8

--- src/action.rs
+++ src/action.rs
@@ -3,11 +3,11 @@
 // last 4-7 are green (positive) actions
 
 pub struct Action {
-    pub points: i8,
+    pub points: i32,
     pub name: &'static str,
 }
 
-pub const ANVIL_ACTIONS: [Action; 8] = [
+pub const COMMON_ACTIONS: [Action; 9] = [
     Action {
         points: -3,
         name: "Light Hit",
@@ -37,7 +37,11 @@ pub const ANVIL_ACTIONS: [Action; 8] = [
         name: "Upset",
     },
     Action {
-        points: 15,
+        points: 16,
         name: "Shrink",
+    },
+    Action {
+        points: 0,
+        name: "None",
     },
 ];
